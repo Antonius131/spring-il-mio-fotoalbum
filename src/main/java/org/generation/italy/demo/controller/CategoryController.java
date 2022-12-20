d1d61fd4fff848d2aa33e89d44ca311fad88cbb6
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-20 16:37:15 +0100

    creating create method in controller

--- src/main/java/org/generation/italy/demo/controller/CategoryController.java
+++ src/main/java/org/generation/italy/demo/controller/CategoryController.java
@@ -3,12 +3,19 @@ package org.generation.italy.demo.controller;
 import java.util.List;
 
 import org.generation.italy.demo.pojo.Category;
+import org.generation.italy.demo.pojo.Photo;
 import org.generation.italy.demo.service.CategoryService;
+import org.generation.italy.demo.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/categories")
@@ -16,6 +23,9 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryService cateService;
+	
+	@Autowired
+	private PhotoService photoService;
 	
 	@GetMapping
 	public String index(Model model) {
@@ -26,6 +36,34 @@ public class CategoryController {
 		model.addAttribute("categories", categories);
 		
 		return "category-index";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		
+		Category category = new Category();
+		List<Photo> photos = photoService.findAll();
+		
+		model.addAttribute("category", category);
+		model.addAttribute("photos", photos);
+		
+		return "category-create";
+	}
+	
+	@PostMapping("/create")
+	public String store(
+			@Valid Category category, 
+			BindingResult bindingResult, 
+			RedirectAttributes redirectAttributes
+		) {
+		
+		if(bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			return "redirect:/photos/create";
+		}
+		
+		cateService.save(category);
+		return "redirect:/categories";
 	}
 	
 }
