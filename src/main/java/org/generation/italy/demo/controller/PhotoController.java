58ee6ee6411bc810f591a225197aab23232b683c
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-20 13:31:25 +0100

    create show selected image method in controller

--- src/main/java/org/generation/italy/demo/controller/PhotoController.java
+++ src/main/java/org/generation/italy/demo/controller/PhotoController.java
@@ -1,13 +1,16 @@
 package org.generation.italy.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.generation.italy.demo.pojo.Category;
 import org.generation.italy.demo.pojo.Photo;
 import org.generation.italy.demo.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +27,21 @@ public class PhotoController {
 		model.addAttribute("photos", photos);
 		
 		return "photo-index";
+	}
+	
+	@GetMapping("/show/{id}")
+	public String show(
+			@PathVariable("id") int id,
+			Model model) {
+		
+		Optional<Photo> optPhoto = photoService.findById(id);
+		
+		Photo photo = optPhoto.get();
+		List<Category> categories = photo.getCategories();
+		
+		model.addAttribute("photo", photo);
+		model.addAttribute("categories", categories);
+		
+		return "photo-show";
 	}
 }
