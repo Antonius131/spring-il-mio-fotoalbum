a94ae5ccc0f0d18843a5710e0c287779536f695c
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-21 15:13:45 +0100

    get categories from db in DOM

--- src/main/java/org/generation/italy/demo/api/controller/CategoryApiController.java
+++ src/main/java/org/generation/italy/demo/api/controller/CategoryApiController.java
@@ -3,24 +3,28 @@ package org.generation.italy.demo.api.controller;
 import java.util.List;
 
 import org.generation.italy.demo.pojo.Category;
-import org.generation.italy.demo.service.CategoryService;
+import org.generation.italy.demo.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("api/1/categories")
+@RequestMapping("api/1")
 @CrossOrigin("*")
 public class CategoryApiController {
-
-	@Autowired
-	private CategoryService cateService;
 	
-	@GetMapping("/all")
-	public List<Category> getCategories() {
+	@Autowired
+	private PhotoService photoService;
+	
+	@GetMapping("/ph/category/{id}")
+	public List<Category> getCategoriesById(@PathVariable("id") int id) {
 		
-		return cateService.findAll();
+		List<Category> categoriesById = photoService.findById(id).get().getCategories();
+		
+		return categoriesById;
 	}
 }
--- vue/src/components/PhotoComp.vue
+++ vue/src/components/PhotoComp.vue
@@ -5,6 +5,14 @@
          <h4>{{ photo.title }}</h4>
          <p>{{ photo.description }}</p>
          <img :src="photo.url" :alt="photo.title">
+         
+         <!-- comments and categories section-->
+         <div @click="getCategories(photo.id)">
+            <p>Vai ai commenti</p>
+            <span v-for="category in categories" :key="category.id">
+               {{ category.name }}
+            </span>
+         </div>
       </div>
    </div>
 </template>
@@ -18,18 +26,39 @@
       data() {
          return {
 
-            apiUrl: 'http://localhost:8080/api/1/photos',
-            photos: []
+            apiUrl: 'http://localhost:8080/api/1',
+            photos: [],
+            categories: []
          }
       },
 
-      mounted() {
-         axios.get(this.apiUrl + "/all")
+      methods: {
+         getPhotos() {
+            axios.get(this.apiUrl + "/photos/all")
             .then(result => {
 
                console.log(result.data);
                this.photos = result.data;
             });
+         },
+         
+         getCategories(photoId) {
+            axios.get(this.apiUrl + "/ph/category/" + photoId)
+            .then(result => {
+
+               console.log(result.data);
+               this.categories = result.data;
+            });
+         },
+
+         getPhotoId(id) {
+
+            console.log(id);
+         }
+      },
+
+      mounted() {
+         this.getPhotos()
       }
    }
 </script>
