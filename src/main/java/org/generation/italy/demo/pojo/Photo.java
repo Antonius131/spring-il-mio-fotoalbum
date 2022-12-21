5d20fcc13f7818b68394ae5b86b703b3c00b5917
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-21 11:10:21 +0100

    add annotations to get clean api calls

--- src/main/java/org/generation/italy/demo/pojo/Photo.java
+++ src/main/java/org/generation/italy/demo/pojo/Photo.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.URL;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -43,6 +45,7 @@ public class Photo {
 	private boolean visible;
 
 	@ManyToMany
+	@JsonIgnore
 	private List<Category> categories;
 	
 	
